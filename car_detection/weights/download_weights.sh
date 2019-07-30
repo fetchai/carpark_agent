309ee3f9b220957cc605a6b6ff7383f3a0e49686
Diarmid Campbell <diarmid.campbell@fetch.ai>
2019-07-30 17:12:05 +0100

    added requiremnts and messed around with some scripts

--- car_detection/weights/download_weights.sh
+++ car_detection/weights/download_weights.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
+
+# cd to directory that this script is in
+script_name=$0
+script_full_path=$(dirname "$0")
+cd "$script_full_path"
+
 wget "https://github.com/matterport/Mask_RCNN/releases/download/v2.0/mask_rcnn_coco.h5"
 
